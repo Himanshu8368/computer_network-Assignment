c392929c0916b7a90f7ef38427c8a7e02e9a91aa
Himanshu Kumar <himanshusinha383@gmail.com>
2024-10-01 20:47:09 +0530

    Add assignment 2

---
+++ build/CMakeFiles/assignment2.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/5th_Sem/Computer Networking/assignment/assignment2/client.c" "CMakeFiles/assignment2.dir/client.c.obj" "gcc" "CMakeFiles/assignment2.dir/client.c.obj.d"
+  "D:/5th_Sem/Computer Networking/assignment/assignment2/q1.c" "CMakeFiles/assignment2.dir/q1.c.obj" "gcc" "CMakeFiles/assignment2.dir/q1.c.obj.d"
+  "D:/5th_Sem/Computer Networking/assignment/assignment2/q2.c" "CMakeFiles/assignment2.dir/q2.c.obj" "gcc" "CMakeFiles/assignment2.dir/q2.c.obj.d"
+  "D:/5th_Sem/Computer Networking/assignment/assignment2/server.c" "CMakeFiles/assignment2.dir/server.c.obj" "gcc" "CMakeFiles/assignment2.dir/server.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/assignment2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment2.dir/client.c.obj"
+  "CMakeFiles/assignment2.dir/client.c.obj.d"
+  "CMakeFiles/assignment2.dir/q1.c.obj"
+  "CMakeFiles/assignment2.dir/q1.c.obj.d"
+  "CMakeFiles/assignment2.dir/q2.c.obj"
+  "CMakeFiles/assignment2.dir/q2.c.obj.d"
+  "CMakeFiles/assignment2.dir/server.c.obj"
+  "CMakeFiles/assignment2.dir/server.c.obj.d"
+  "assignment2.exe"
+  "assignment2.exe.manifest"
+  "assignment2.pdb"
+  "libassignment2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assignment2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
